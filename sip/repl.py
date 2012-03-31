a593350f0e1ac2cd0b80fbe7acc55e61e0308ae8
Guillaume Bour <guillaume@bour.cc>
2012-03-31 09:24:28 +0100

    [feat] REPL: handle commands history with up/down keys

--- sip/repl.py
+++ sip/repl.py
@@ -60,10 +60,21 @@ class Repl(asyncore.file_dispatcher):
             "[24~", # f12
         ]
 
+        # history
+        self.histf = open('.history', 'a+')
+
+        #NOTE: we only keep last 100 commands
+        self.history = [line[:-1] for line in self.histf.readlines()]
+        if len(self.history) > 100:
+            self.histf.seek(0)
+            self.history = self.history[-100:]
+        self.curhist = None
+
 
     def cleanup(self):
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
         self.logf.close()
+        self.histf.close()
 
     def handle_read(self):
         c = self.recv(1)
@@ -71,6 +82,32 @@ class Repl(asyncore.file_dispatcher):
         if self.ansi_mode:
             self.ansi_buffer += c
             if self.ansi_buffer in self.sequences:
+
+                if self.ansi_buffer == '[A' and len(self.history) > 0: # up arrow:
+                    if self.curhist is None:
+                        self.curhist = len(self.history)-1
+
+                        if len(self.buffer) > 0:
+                            self.history.append(''.join(self.buffer))
+                    elif self.curhist > 0:
+                        self.history[self.curhist] = ''.join(self.buffer)
+                        self.curhist -= 1
+
+                    if self.curhist >= 0:
+                        # erase line (5 == prompt width)
+                        sys.stdout.write('\r' + ' '*(5+len(''.join(self.buffer).expandtabs())) + '\r')
+                        self.buffer = list(self.history[self.curhist])
+                        self.prompt(''.join(self.buffer))
+
+                elif self.ansi_buffer == '[B' and self.curhist is not None and\
+                        self.curhist < len(self.history)-1:
+                        sys.stdout.write('\r' + ' '*(5+len(''.join(self.buffer).expandtabs())) + '\r')
+
+                        self.curhist += 1
+                        self.buffer = list(self.history[self.curhist])
+                        self.prompt(''.join(self.buffer))
+
+
                 self.ansi_mode = False
 
             return
@@ -82,11 +119,16 @@ class Repl(asyncore.file_dispatcher):
         # carriage return
         elif c == '\n':
             # do the job
-            self.callback(''.join(self.buffer))
+            cmd = ''.join(self.buffer).strip()
+
+            if len(cmd) > 0:
+                self.curhist = None
+                self.add_history(cmd)
+                self.callback(cmd)
 
             # flush buffer
             del self.buffer[:]
-            self.prompt(); return
+            self.prompt(newline=True); return
 
         # backspace
         elif c == '\b' or ord(c) == 127:
@@ -101,7 +143,7 @@ class Repl(asyncore.file_dispatcher):
         self.buffer.append(c)
 
     def echo(self, data, flush=False):
-        sys.stdout.write('\n'+data)
+        sys.stdout.write('\n'+data+'\n')
 
         if flush:
             self.flush()
@@ -109,10 +151,17 @@ class Repl(asyncore.file_dispatcher):
     def flush(self):
             self.prompt(''.join(self.buffer))
 
-    def prompt(self, content=''):
-        sys.stdout.write("\n[.]> "+content)
+    def prompt(self, content='', newline=False):
+        if newline:
+            sys.stdout.write('\n')
+        sys.stdout.write("[.]> "+content)
 
 
     def debug(self, data):
         self.logf.write(data)
         self.echo(data)
+
+    def add_history(self, command):
+        self.history.append(command)
+        self.histf.write(command+'\n')
+
